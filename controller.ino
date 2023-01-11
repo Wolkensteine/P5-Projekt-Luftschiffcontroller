7918d737dd5d9d3fc7e2fc3ab361be03c18d7a78
Jakob <105732359+Wolkensteine@users.noreply.github.com>
2023-01-11 15:09:05 +0100

    Initial imports and additions

---
+++ controller.ino
@@ -0,0 +1,33 @@
+/*
+ * This file contains the code for the ESP32 (Node-MCU) for our P5-Project
+ * The code in this not only controlls the airship, but also servers a web server over WiFi for controlling it remotly. 
+ * Further it serves the purpose to transmit, and save data gathered by the sensors on board.
+*/
+
+
+// Imports
+#include <Arduino.h>
+#include <DHT.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <MQ135.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+// Setting up DHT11 sensor (temperature and humidity)
+#define WIRE Wire
+#define DHTPIN 16
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// Setting up MQ135 (CO2/Airquality sensor)
+int val;
+int sensorPin = 34;
+int sensorValue = 0;
+MQ135 gasSensor = MQ135(sensorPin, 54.51, 22); 
+
+// Setting up bmp (Pressure sensor)
+Adafruit_BMP085 bmp;
